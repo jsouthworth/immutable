effb4f500af2c87e9aad4cf9dda3e2c780047bc7
John Southworth <john.southworth@gmail.com>
2019-02-09 07:56:48 -0800

    stack: initial implementation

---
+++ stack/stack.go
@@ -0,0 +1,287 @@
+// Package stack implements a persistent stack.
+package stack // import "jsouthworth.net/go/immutable/stack"
+
+import (
+	"errors"
+	"fmt"
+	"reflect"
+	"strings"
+
+	"jsouthworth.net/go/immutable/vector"
+	"jsouthworth.net/go/seq"
+)
+
+var errRangeSig = errors.New("Range requires a function: func(v vT) bool or func(v vT)")
+
+// Stack is a persistent stack.
+type Stack struct {
+	backingVector *vector.Vector
+}
+
+var empty = Stack{
+	backingVector: vector.Empty(),
+}
+
+// Empty returns the empty stack.
+func Empty() *Stack {
+	return &empty
+}
+
+// New converts as list of elements to a persistent stack.
+func New(elems ...interface{}) *Stack {
+	out := Empty().AsTransient()
+	for _, elem := range elems {
+		out = out.Push(elem)
+	}
+	return out.AsPersistent()
+}
+
+// From will convert many go types to an immutable map.
+// Converting some types is more efficient than others and the
+// mechanisms are described below.
+//
+// *Stack:
+//    Used directly as it is already immutable.
+// *TStack:
+//    AsPersistent is called on the value and the result used for the stack.
+// *Vector:
+//    Used directly as the stack as it is already immutable.
+// *TVector:
+//    AsPersistent is called on the value and the result used for the stack.
+// []interface{}:
+//    New is called with the elements.
+// seq.Sequable:
+//    Seq is called on the value and the stack is built from the resulting sequence.
+// seq.Sequence:
+//    The stack is built from the sequence. Care should be taken to provide finite sequences or the vector will grow without bound.
+// []T:
+//    The slice is converted to a vector using reflection.
+func From(value interface{}) *Stack {
+	switch v := value.(type) {
+	case *Stack:
+		return v
+	case *TStack:
+		return v.AsPersistent()
+	default:
+		vec := vector.From(value)
+		if vec.Length() == 0 {
+			return Empty()
+		}
+		return &Stack{
+			backingVector: vec,
+		}
+	}
+}
+
+// Push returns a new stack with the element as the top of the stack.
+func (s *Stack) Push(elem interface{}) *Stack {
+	return &Stack{
+		backingVector: s.backingVector.Append(elem),
+	}
+}
+
+// Pop returns a new stack without the top element
+func (s *Stack) Pop() *Stack {
+	v := s.backingVector.Pop()
+	if v.Length() == 0 {
+		return Empty()
+	}
+	return &Stack{
+		backingVector: v,
+	}
+}
+
+// Top returns the top of the stack
+func (s *Stack) Top() interface{} {
+	return s.backingVector.At(s.backingVector.Length() - 1)
+}
+
+// AsTransient will return a mutable copy on write version of the stack.
+func (s *Stack) AsTransient() *TStack {
+	return &TStack{
+		backingVector: s.backingVector.AsTransient(),
+	}
+}
+
+// Range calls the passed in function on each element of the stack.
+// The function passed in may be of many types:
+//
+// func(value interface{}) bool:
+//    Takes a value of any type and returns if the loop should continue.
+//    Useful to avoid reflection where not needed and to support
+//    heterogenous stacks.
+// func(value interface{})
+//    Takes a value of any type.
+//    Useful to avoid reflection where not needed and to support
+//    heterogenous stacks.
+// func(value T) bool:
+//    Takes a value of the type of element stored in the stack and
+//    returns if the loop should continue. Useful for homogeneous stacks.
+//    Is called with reflection and will panic if the type is incorrect.
+// func(value T)
+//    Takes a value of the type of element stored in the stack and
+//    returns if the loop should continue. Useful for homogeneous stacks.
+//    Is called with reflection and will panic if the type is incorrect.
+// Range will panic if passed anything that doesn't match one of these signatures
+func (s *Stack) Range(do interface{}) {
+	cont := true
+	for stack := s; stack != Empty() && cont; stack = stack.Pop() {
+		value := stack.Top()
+		switch fn := do.(type) {
+		case func(value interface{}) bool:
+			cont = fn(value)
+		case func(value interface{}):
+			fn(value)
+		default:
+			rv := reflect.ValueOf(do)
+			if rv.Kind() != reflect.Func {
+				panic(errRangeSig)
+			}
+			rt := rv.Type()
+			if rt.NumIn() != 1 || rt.NumOut() > 1 {
+				panic(errRangeSig)
+			}
+			if rt.NumOut() == 1 &&
+				rt.Out(0).Kind() != reflect.Bool {
+				panic(errRangeSig)
+			}
+			outs := rv.Call([]reflect.Value{
+				reflect.ValueOf(value)})
+			if len(outs) != 0 {
+				cont = outs[0].Interface().(bool)
+			}
+		}
+	}
+}
+
+// Seq returns a representation of the stack as a sequence
+// corresponding to the elements of the stack.
+func (s *Stack) Seq() seq.Sequence {
+	return &stackSequence{
+		stack: s,
+	}
+}
+
+// String returns a representation of the stack as a string.
+func (s *Stack) String() string {
+	b := new(strings.Builder)
+	fmt.Fprint(b, "[ ")
+	s.Range(func(item interface{}) {
+		fmt.Fprintf(b, "%v ", item)
+	})
+	fmt.Fprint(b, "]")
+	return b.String()
+}
+
+type stackSequence struct {
+	stack *Stack
+}
+
+func (s *stackSequence) First() interface{} {
+	return s.stack.Top()
+}
+
+func (s *stackSequence) Next() seq.Sequence {
+	new := s.stack.Pop()
+	if new.backingVector.Length() == 0 {
+		return nil
+	}
+	return &stackSequence{
+		stack: new,
+	}
+}
+
+type TStack struct {
+	backingVector *vector.TVector
+}
+
+// Push places an element at the top of the stack. s is returned
+func (s *TStack) Push(elem interface{}) *TStack {
+	s.backingVector = s.backingVector.Append(elem)
+	return s
+}
+
+// Pop removes the top element of the stack. s is returned.
+func (s *TStack) Pop() *TStack {
+	s.backingVector = s.backingVector.Pop()
+	return s
+}
+
+// Top returns the top element of the stack.
+func (s *TStack) Top() interface{} {
+	return s.backingVector.At(s.backingVector.Length() - 1)
+}
+
+// AsPersistent returns the an immutable version of the stack. Any
+// transient operations performed after this will cause a panic.
+func (s *TStack) AsPersistent() *Stack {
+	v := s.backingVector.AsPersistent()
+	if v.Length() == 0 {
+		return Empty()
+	}
+	return &Stack{
+		backingVector: v,
+	}
+}
+
+// Range calls the passed in function on each element of the stack.
+// The function passed in may be of many types:
+//
+// func(value interface{}) bool:
+//    Takes a value of any type and returns if the loop should continue.
+//    Useful to avoid reflection where not needed and to support
+//    heterogenous stacks.
+// func(value interface{})
+//    Takes a value of any type.
+//    Useful to avoid reflection where not needed and to support
+//    heterogenous stacks.
+// func(value T) bool:
+//    Takes a value of the type of element stored in the stack and
+//    returns if the loop should continue. Useful for homogeneous stacks.
+//    Is called with reflection and will panic if the type is incorrect.
+// func(value T)
+//    Takes a value of the type of element stored in the stack and
+//    returns if the loop should continue. Useful for homogeneous stacks.
+//    Is called with reflection and will panic if the type is incorrect.
+// Range will panic if passed anything that doesn't match one of these signatures
+func (s *TStack) Range(do interface{}) {
+	cont := true
+	for i := s.backingVector.Length() - 1; i >= 0 && cont; i-- {
+		value := s.backingVector.At(i)
+		switch fn := do.(type) {
+		case func(value interface{}) bool:
+			cont = fn(value)
+		case func(value interface{}):
+			fn(value)
+		default:
+			rv := reflect.ValueOf(do)
+			if rv.Kind() != reflect.Func {
+				panic(errRangeSig)
+			}
+			rt := rv.Type()
+			if rt.NumIn() != 1 || rt.NumOut() > 1 {
+				panic(errRangeSig)
+			}
+			if rt.NumOut() == 1 &&
+				rt.Out(0).Kind() != reflect.Bool {
+				panic(errRangeSig)
+			}
+			outs := rv.Call([]reflect.Value{
+				reflect.ValueOf(value)})
+			if len(outs) != 0 {
+				cont = outs[0].Interface().(bool)
+			}
+		}
+	}
+}
+
+// String returns a representation of the stack as a string.
+func (s *TStack) String() string {
+	b := new(strings.Builder)
+	fmt.Fprint(b, "[ ")
+	s.Range(func(item interface{}) {
+		fmt.Fprintf(b, "%v ", item)
+	})
+	fmt.Fprint(b, "]")
+	return b.String()
+}
---
+++ stack/stack_test.go
@@ -0,0 +1,395 @@
+package stack
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/leanovate/gopter"
+	"github.com/leanovate/gopter/gen"
+	"github.com/leanovate/gopter/prop"
+	"jsouthworth.net/go/seq"
+)
+
+func TestStack(t *testing.T) {
+	parameters := gopter.DefaultTestParameters()
+	properties := gopter.NewProperties(parameters)
+	properties.Property("s=Empty().Push(a) -> s.Top()==a and s.Pop()==Empty()",
+		prop.ForAll(
+			func(a int) bool {
+				s := Empty().Push(a)
+				return s.Top() == a && s.Pop() == Empty()
+			},
+			gen.Int(),
+		))
+	properties.Property("s=New(is).Push(a) -> s.Top()==a)",
+		prop.ForAll(
+			func(as []interface{}, a int) bool {
+				s := New(as...).Push(a)
+				return s.Top() == a
+			},
+			gen.SliceOf(gen.Int(),
+				reflect.TypeOf((*interface{})(nil)).Elem()),
+			gen.Int(),
+		))
+	properties.Property("s=New(is).Push(a) -> s.Pop() != Empty())",
+		prop.ForAll(
+			func(as []interface{}, a int) bool {
+				s := New(as...).Push(a)
+				return s.Pop() != Empty()
+			},
+			gen.SliceOfN(10, gen.Int(),
+				reflect.TypeOf((*interface{})(nil)).Elem()),
+			gen.Int(),
+		))
+	properties.Property("s=From(is).Push(a) -> s.Pop() != Empty())",
+		prop.ForAll(
+			func(as []interface{}, a int) bool {
+				s := From(as).Push(a)
+				return s.Pop() != Empty()
+			},
+			gen.SliceOfN(10, gen.Int(),
+				reflect.TypeOf((*interface{})(nil)).Elem()),
+			gen.Int(),
+		))
+	properties.Property("s=From(empty).Push(a) -> s.Pop() == Empty())",
+		prop.ForAll(
+			func(as []interface{}, a int) bool {
+				s := From(as).Push(a)
+				return s.Pop() == Empty()
+			},
+			gen.SliceOfN(0, gen.Int(),
+				reflect.TypeOf((*interface{})(nil)).Elem()),
+			gen.Int(),
+		))
+	properties.TestingRun(t)
+}
+
+func TestString(t *testing.T) {
+	s := Empty().Push(1).Push(2).Push(3)
+	str := s.String()
+	expected := "[ 3 2 1 ]"
+	if str != expected {
+		t.Fatalf("got %s, expected %s", str, expected)
+	}
+	tr := Empty().AsTransient().Push(1).Push(2).Push(3)
+	str = tr.String()
+	if str != expected {
+		t.Fatalf("transient: got %s, expected %s", str, expected)
+	}
+}
+
+func TestSeq(t *testing.T) {
+	parameters := gopter.DefaultTestParameters()
+	properties := gopter.NewProperties(parameters)
+	properties.Property("Reduce iterates all elements",
+		prop.ForAll(
+			func(as []int) bool {
+				var expected int
+				for _, a := range as {
+					expected += a
+				}
+				s := From(as)
+				got := seq.Reduce(func(res, in int) int {
+					return res + in
+				}, 0, seq.Seq(s)).(int)
+
+				return got == expected
+			},
+			gen.SliceOfN(10, gen.Int()),
+		))
+	properties.TestingRun(t)
+}
+
+func TestTStack(t *testing.T) {
+	parameters := gopter.DefaultTestParameters()
+	properties := gopter.NewProperties(parameters)
+	properties.Property("s=Empty().Push(a) -> s.Top()==a and s.Pop()==Empty()",
+		prop.ForAll(
+			func(a int) bool {
+				s := Empty().AsTransient().Push(a)
+				return s.Top() == a && s.Pop().AsPersistent() == Empty()
+			},
+			gen.Int(),
+		))
+	properties.Property("s=New(is).Push(a) -> s.Top()==a)",
+		prop.ForAll(
+			func(as []interface{}, a int) bool {
+				s := New(as...).AsTransient().Push(a)
+				return s.Top() == a
+			},
+			gen.SliceOf(gen.Int(),
+				reflect.TypeOf((*interface{})(nil)).Elem()),
+			gen.Int(),
+		))
+	properties.Property("s=New(is).Push(a) -> s.Pop() != Empty())",
+		prop.ForAll(
+			func(as []interface{}, a int) bool {
+				s := New(as...).AsTransient().Push(a)
+				return s.Pop().AsPersistent() != Empty()
+			},
+			gen.SliceOfN(10, gen.Int(),
+				reflect.TypeOf((*interface{})(nil)).Elem()),
+			gen.Int(),
+		))
+	properties.TestingRun(t)
+}
+
+func TestRange(t *testing.T) {
+	parameters := gopter.DefaultTestParameters()
+	properties := gopter.NewProperties(parameters)
+	properties.Property("Range func(interface{})",
+		prop.ForAll(
+			func(a int) bool {
+				expected := a + a
+				l := Empty().Push(a).Push(a)
+				var got int
+				l.Range(func(i interface{}) {
+					got += i.(int)
+				})
+				return got == expected
+			},
+			gen.Int(),
+		))
+	properties.Property("Range func(interface{}) bool",
+		prop.ForAll(
+			func(a int) bool {
+				expected := a
+				l := Empty().Push(a).Push(a)
+				var got int
+				l.Range(func(i interface{}) bool {
+					got += i.(int)
+					return false
+				})
+				return got == expected
+			},
+			gen.Int(),
+		))
+	properties.Property("Range func(T)",
+		prop.ForAll(
+			func(a int) bool {
+				expected := a + a
+				l := Empty().Push(a).Push(a)
+				var got int
+				l.Range(func(i int) {
+					got += i
+				})
+				return got == expected
+			},
+			gen.Int(),
+		))
+	properties.Property("Range func(T) bool",
+		prop.ForAll(
+			func(a int) bool {
+				expected := a
+
+				l := Empty().Push(a).Push(a)
+				var got int
+				l.Range(func(i int) bool {
+					got += i
+					return false
+				})
+				return got == expected
+			},
+			gen.Int(),
+		))
+	properties.Property("Range func(T) T panics",
+		prop.ForAll(
+			func(a int) (ok bool) {
+				defer func() {
+					r := recover()
+					ok = r == errRangeSig
+				}()
+				expected := a
+				l := Empty().Push(a).Push(a)
+				var got int
+				l.Range(func(i int) int {
+					got += i
+					return got
+				})
+				return got == expected
+			},
+			gen.Int(),
+		))
+	properties.Property("Range func(T, T) bool panics",
+		prop.ForAll(
+			func(a int) (ok bool) {
+				defer func() {
+					r := recover()
+					ok = r == errRangeSig
+				}()
+				expected := a
+				l := Empty().Push(a).Push(a)
+				var got int
+				l.Range(func(i, j int) bool {
+					got += i
+					return true
+				})
+				return got == expected
+			},
+			gen.Int(),
+		))
+	properties.Property("Range func(T, T) (bool,bool) panics",
+		prop.ForAll(
+			func(a int) (ok bool) {
+				defer func() {
+					r := recover()
+					ok = r == errRangeSig
+				}()
+				expected := a
+				l := Empty().Push(a).Push(a)
+				var got int
+				l.Range(func(i, j int) (bool, bool) {
+					got += i
+					return true, false
+				})
+				return got == expected
+			},
+			gen.Int(),
+		))
+	properties.Property("Range(int) panics",
+		prop.ForAll(
+			func(a int) (ok bool) {
+				defer func() {
+					r := recover()
+					ok = r == errRangeSig
+				}()
+				expected := a
+				l := Empty().Push(a).Push(a)
+				var got int
+				l.Range(a)
+				return got == expected
+			},
+			gen.Int(),
+		))
+	properties.TestingRun(t)
+}
+
+func TestTransientRange(t *testing.T) {
+	parameters := gopter.DefaultTestParameters()
+	properties := gopter.NewProperties(parameters)
+	properties.Property("Range func(interface{})",
+		prop.ForAll(
+			func(a int) bool {
+				expected := a + a
+				l := Empty().AsTransient().Push(a).Push(a)
+				var got int
+				l.Range(func(i interface{}) {
+					got += i.(int)
+				})
+				return got == expected
+			},
+			gen.Int(),
+		))
+	properties.Property("Range func(interface{}) bool",
+		prop.ForAll(
+			func(a int) bool {
+				expected := a
+				l := Empty().AsTransient().Push(a).Push(a)
+				var got int
+				l.Range(func(i interface{}) bool {
+					got += i.(int)
+					return false
+				})
+				return got == expected
+			},
+			gen.Int(),
+		))
+	properties.Property("Range func(T)",
+		prop.ForAll(
+			func(a int) bool {
+				expected := a + a
+				l := Empty().AsTransient().Push(a).Push(a)
+				var got int
+				l.Range(func(i int) {
+					got += i
+				})
+				return got == expected
+			},
+			gen.Int(),
+		))
+	properties.Property("Range func(T) bool",
+		prop.ForAll(
+			func(a int) bool {
+				expected := a
+
+				l := Empty().AsTransient().Push(a).Push(a)
+				var got int
+				l.Range(func(i int) bool {
+					got += i
+					return false
+				})
+				return got == expected
+			},
+			gen.Int(),
+		))
+	properties.Property("Range func(T) T panics",
+		prop.ForAll(
+			func(a int) (ok bool) {
+				defer func() {
+					r := recover()
+					ok = r == errRangeSig
+				}()
+				expected := a
+				l := Empty().AsTransient().Push(a).Push(a)
+				var got int
+				l.Range(func(i int) int {
+					got += i
+					return got
+				})
+				return got == expected
+			},
+			gen.Int(),
+		))
+	properties.Property("Range func(T, T) bool panics",
+		prop.ForAll(
+			func(a int) (ok bool) {
+				defer func() {
+					r := recover()
+					ok = r == errRangeSig
+				}()
+				expected := a
+				l := Empty().AsTransient().Push(a).Push(a)
+				var got int
+				l.Range(func(i, j int) bool {
+					got += i
+					return true
+				})
+				return got == expected
+			},
+			gen.Int(),
+		))
+	properties.Property("Range func(T, T) (bool,bool) panics",
+		prop.ForAll(
+			func(a int) (ok bool) {
+				defer func() {
+					r := recover()
+					ok = r == errRangeSig
+				}()
+				expected := a
+				l := Empty().AsTransient().Push(a).Push(a)
+				var got int
+				l.Range(func(i, j int) (bool, bool) {
+					got += i
+					return true, false
+				})
+				return got == expected
+			},
+			gen.Int(),
+		))
+	properties.Property("Range(int) panics",
+		prop.ForAll(
+			func(a int) (ok bool) {
+				defer func() {
+					r := recover()
+					ok = r == errRangeSig
+				}()
+				expected := a
+				l := Empty().AsTransient().Push(a).Push(a)
+				var got int
+				l.Range(a)
+				return got == expected
+			},
+			gen.Int(),
+		))
+	properties.TestingRun(t)
+}
