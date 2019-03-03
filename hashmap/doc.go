c02e32e91f0e74afc37745220d89d202b93c829e
John Southworth <john.southworth@gmail.com>
2019-03-02 16:32:35 -0800

    hashmap: document equality requirements

--- hashmap/doc.go
+++ hashmap/doc.go
@@ -2,4 +2,9 @@
 // package is inspired by on Clojure's persistent and transient hashmap
 // implementations. See https://lampwww.epfl.ch/papers/idealhashtrees.pdf
 // for more information on the algoritm.
+//
+// A note about Key and Value equality. If you would like to override
+// the default go equality operator for keys and values in this map library
+// implement the Equal(other interface{}) bool function for the type.
+// Otherwise '==' will be used with all its restrictions.
 package hashmap
