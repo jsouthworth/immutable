7f1438a719b92144ba2193f3f77ee02ca1a1b8f1
John Southworth <john.southworth@gmail.com>
2019-03-02 16:37:45 -0800

    treemap: document equality requirements

--- treemap/doc.go
+++ treemap/doc.go
@@ -3,4 +3,11 @@
 // deletion extension. See:
 // http://www.eecs.usma.edu/webs/people/okasaki/jfp99.ps and
 // http://matt.might.net/papers/germane2014deletion.pdf for details.
+//
+// A note about Key and Value equality. If you would like to override
+// the default go equality operator for keys and values in this map library
+// implement the Equal(other interface{}) bool function for the type.
+// Otherwise '==' will be used with all its restrictions. Additionally,
+// Key's must be comparable. One may implement Compare(other interface{}) int
+// to override the default comparable restrcitions.
 package treemap
